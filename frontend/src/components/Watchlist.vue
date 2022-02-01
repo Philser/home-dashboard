323f85b1dd58d3dac1cbe693a691d54cf35a4849
Philser <Pk30.05@hotmail.de>
2022-02-01 19:49:23 +0100

    Add a stupid little component

---
+++ frontend/src/components/Watchlist.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="watchlist">
+    <input type="text" id="movName" name="movName" placeholder="Movie Name" />
+    <button class="submitMovie" @click="submitMovie">Submit</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    msg: String,
+  },
+})
+export default class Watchlist extends Vue {
+  msg!: string;
+
+  submitMovie(): void {
+    window.alert(this.msg);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Watchlist />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Watchlist from '@/components/Watchlist.vue'; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+    Watchlist,
   },
 })
 export default class Home extends Vue {}
