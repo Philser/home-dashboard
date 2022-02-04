780e4f8c4f5b37944786fa2bc76eb2db995579af
Philser <Pk30.05@hotmail.de>
2022-02-04 19:00:44 +0100

    Revert "- Upgrade to node 14"
    This reverts commit 005c217991fae109d61184e45c7e013866a399aa.

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <hello-world />
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Watchlist />
+    <ShoppingList />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import Watchlist from '../components/Watchlist.vue'; // @ is an alias to /src
-import ShoppingList from '../components/ShoppingList.vue'; // @ is an alias to /src
+import { Options, Vue } from 'vue-class-component';
+import Watchlist from '@/components/Watchlist.vue'; // @ is an alias to /src
+import ShoppingList from '@/components/ShoppingList.vue'; // @ is an alias to /src
 
-export default defineComponent({
-  name: 'Home',
-
+@Options({
   components: {
     Watchlist,
     ShoppingList,
   },
-});
+})
+export default class Home extends Vue {}
 </script>
