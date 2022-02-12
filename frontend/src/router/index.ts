638f8a235956556c7d065ca350f2d229e778e1e5
Philser <Pk30.05@hotmail.de>
2022-02-12 17:01:35 +0100

    Add route guard

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,7 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue';
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import { authState } from '../auth'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -15,11 +17,28 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
-];
+  {
+    path: '/login',
+    name: 'Login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Login,
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-});
+})
 
-export default router;
+router.beforeEach((to, _) => {
+  console.log(to)
+  if (to.path !== '/login') {
+    if (!authState.loggedIn) {
+      router.push('/login')
+    }
+  }
+})
+
+export default router
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container> </v-container>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({})
+export default class LoginComponent extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
