1dba6251e500a1a493e26ff57a0351101e2500f3
Philser <Pk30.05@hotmail.de>
2022-02-27 19:31:25 +0100

    - Add backend dockerfile - Add db config parsing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,12 +6,9 @@ import { initDb } from './db/Mongo'
 import { wachlistApi } from './api/endpoints/Watchlist'
 import { ShoppingListHandler as shoppingListApi } from './api/endpoints/ShoppingList'
 import { LoginHandler as loginHandler } from './api/endpoints/Login'
-import { Config } from './config'
-import * as fs from 'fs'
-import { dirname } from 'path'
+import { Config, parseConfig } from './config'
 
 const app = express()
-const DEFAULT_PORT = 8081 // default port to listen
 
 function initMiddlewares(config: Config) {
     app.use(cors({
@@ -26,57 +23,11 @@ function initMiddlewares(config: Config) {
     app.use(cookieParser())
 }
 
-function parsePublicPrivateKeys(): { publicKeyPem: string, privateKeyPem: string } {
-    // TODO: Have a configurable absolute path for keys
-    const appDir = dirname(require.main.filename)
-    const publicKeyPem = fs.readFileSync(`${appDir}/../keys/public.pem`).toString()
-    const privateKeyPem = fs.readFileSync(`${appDir}/../keys/private.pem`).toString()
-
-    return {
-        publicKeyPem,
-        privateKeyPem
-    }
-}
-
-function parseConfig(): Config {
-    try {
-        let port = DEFAULT_PORT
-        if (process.env.PORT) {
-            port = parseInt(process.env.PORT)
-
-            if (port == NaN) {
-                throw new Error(`Invalid PORT value: ${process.env.PORT}. Expected number`)
-            }
-        }
-
-        const domain = process.env.DOMAIN
-        if (!domain) {
-            // TODO: Add validation
-            throw new Error("Missing domain")
-        }
-
-        const keys = parsePublicPrivateKeys()
-        const publicKeyPem = keys.publicKeyPem
-        const privateKeyPem = keys.privateKeyPem
-
-
-        return {
-            port,
-            publicKeyPem,
-            privateKeyPem,
-            domain,
-        }
-    } catch (e) {
-        throw new Error(`Error parsing config: ${e}`)
-    }
-}
-
-
 async function server(): Promise<void> {
     try {
         const config = parseConfig()
 
-        await initDb()
+        await initDb(config)
 
         initMiddlewares(config)
 
