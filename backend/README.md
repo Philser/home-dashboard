b990768c7438add533b661ac160c8cad598471af
Philser <pk30.05@hotmail.de>
2023-01-15 16:32:45 -0500

    fix login service, add readme, cleanup compose

---
+++ backend/README.md
@@ -0,0 +1,29 @@
+# Setup
+
+To run the backend via Docker, you need to provide the following Env Vars:
+
+#### DOMAIN
+Domain to enable CORS for. Usually the URL of the website (including protocol), e.g. https://dashboard.philser.de
+
+#### DB_USER
+User name to use for authenticating with the MongoDB container
+
+#### DB_PASSWORD
+Password to use for authenticating with the MongoDB container
+
+#### PORT
+Port to listen on
+
+#### DOCKERDIR
+Path to the base of two external volumes that are being included into the container:
+- `${DOCKERDIR}/dashboard/mongo`
+    - Used for storing MongoDB data on disk
+- `${DOCKERDIR}/dashboard/db/`
+    - Used to store startup scripts (in `.js` or `.sh` format) for the DB
+- `${DOCKERDIR}/dashboard/keys`
+    - Used to store the SSL certificate for HTTPS support and the private key
+        - `${DOCKERDIR}/dashboard/keys/cert.pem`
+        - `${DOCKERDIR}/dashboard/keys/certKey.pem`
+    - Used to store the RSA keypair for creating JWTs
+        - `${DOCKERDIR}/dashboard/keys/private.pem`
+        - `${DOCKERDIR}/dashboard/keys/public.pem`
--- backend/src/api/endpoints/Login.ts
+++ backend/src/api/endpoints/Login.ts
@@ -1,43 +1,44 @@
 // tslint:disable:no-console
 
-import { Express } from 'express'
-import * as jwt from 'jsonwebtoken'
-import { UserModel } from '../../model/User'
-import * as bcrypt from 'bcrypt'
-import { InternalServerError } from '../errors/Utils'
-import { Config } from '../../config'
+import { Express } from 'express';
+import * as jwt from 'jsonwebtoken';
+import { UserModel } from '../../model/User';
+import * as bcrypt from 'bcrypt';
+import { InternalServerError } from '../errors/Utils';
+import { Config } from '../../config';
 
 export function LoginHandler(app: Express, config: Config) {
     app.post('/api/login', async (req, res) => {
         try {
-            const { username, password } = req.body
+            const { username, password } = req.body;
 
-            const savedUser = await UserModel.findOne({ username }).exec()
+            const savedUser = await UserModel.findOne({ username }).exec();
 
             if (!savedUser) {
                 // TODO: Proper error message body
-                console.error(`Invalid login attempt through unknown user: ${username}`)
-                res.sendStatus(401)
-                return
+                console.error(`Invalid login attempt through unknown user: ${username}`);
+                res.sendStatus(401);
+                return;
             }
 
-            const isValid = await bcrypt.compare(password, savedUser.passwordHash)
+            const isValid = await bcrypt.compare(password, savedUser.passwordHash);
             if (!isValid) {
-                res.sendStatus(401)
+                res.sendStatus(401);
+                return;
             }
 
-            const token = jwt.sign({}, config.privateKeyPem, { algorithm: 'RS256' })
-            console.log('Signed')
+            const token = jwt.sign({}, config.privateKeyPem, { algorithm: 'RS256' });
+            console.log('Signed');
 
             res.cookie('token', `${token}`, {
                 sameSite: 'none',
                 httpOnly: true,
                 secure: true,
-            })
-            res.sendStatus(200)
+            });
+            res.sendStatus(200);
 
         } catch (e) {
-            InternalServerError(res, e, 'POST', '/login')
+            InternalServerError(res, e, 'POST', '/login');
         }
-    })
+    });
 }
